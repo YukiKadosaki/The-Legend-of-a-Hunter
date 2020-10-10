da4c5ba71203694cea25880f264103d792dcb4e9
YukiKadosaki <61261945+YukiKadosaki@users.noreply.github.com>
2020-10-10 21:28:57 +0900

    first

---
+++ Assets/Sample/Maze/MazeGenerator.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [SerializeField] private int m_array_size;
+    [SerializeField] private float m_space;
+    [SerializeField] private GameObject m_wall_prefab;
+    private GameObject[,] m_wall_objects;
+    private bool[,] m_maze_table;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_maze_table = new bool[m_array_size, m_array_size];
+        m_wall_objects = new GameObject[m_array_size, m_array_size];
+        MakeMaze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DeleteWalls();
+            MakeMaze();
+        }
+    }
+
+    private void MakeMaze()
+    {
+        MakeMazeTable();
+
+        for (int i = 0; i < m_array_size; i++)
+        {
+            for (int j = 0; j < m_array_size; j++)
+            {
+                if (m_maze_table[i, j])
+                {
+                    m_wall_objects[i, j] = Instantiate(m_wall_prefab);
+                    m_wall_objects[i, j].transform.position = new Vector3(j * m_space, 0, i * m_space);
+                }
+                else
+                {
+                    m_wall_objects[i, j] = null;
+                }
+            }
+        }
+    }
+
+    private void MakeMazeTable()
+    {
+        InitMazeTable();
+        MakeWallTable();
+        MakeStick();
+        StickDownAll();
+    }
+
+    private void InitMazeTable()
+    {
+        for (int i = 0; i < m_array_size; i++)
+        {
+            for (int j = 0; j < m_array_size; j++)
+            {
+                m_maze_table[i, j] = false;
+            }
+        }
+    }
+
+    private void MakeWallTable()
+    {
+
+        for (int i = 0; i < m_array_size; i++)
+        {
+            m_maze_table[0, i] = true;
+        }
+
+
+
+        for (int i = 0; i < m_array_size; i++)
+        {
+            for (int j = 0; j < m_array_size; j += (m_array_size - 1))
+            {
+                m_maze_table[i, j] = true;
+            }
+        }
+
+
+
+        for (int i = 0; i < m_array_size; i++)
+        {
+            m_maze_table[m_array_size - 1, i] = true;
+        }
+
+    }
+
+    private void MakeStick()
+    {
+        for (int i = 2; i < m_array_size; i += 2)
+        {
+            for (int j = 2; j < m_array_size; j += 2)
+            {
+                m_maze_table[i, j] = true;
+            }
+        }
+    }
+
+    private void StickDownAll()
+    {
+        //周りを壁で囲んでいるのでループの範囲は-1
+        for (int i = 2; i < m_array_size - 1; i += 2)
+        {
+            for (int j = 2; j < m_array_size - 1; j += 2)
+            {
+                int direction;
+
+                //一番上の段だけ上にも倒せる
+                if (i == 2)
+                {
+                    //一番上の段だけ上にも倒せる
+                    direction = Random.Range(1, 4);
+                    StickDown(direction, i, j);
+                }
+                else
+                {
+                    direction = Random.Range(1, 3);
+                    StickDown(direction, i, j);
+                }
+
+            }
+        }
+    }
+
+    private void StickDown(int dir, int i, int j)
+    {
+        if (dir == 1)
+        {
+            m_maze_table[i, ++j] = true;
+        }
+        else if (dir == 2)
+        {
+            m_maze_table[i + 1, j] = true;
+        }
+        else if (dir == 3)
+        {
+            m_maze_table[i, --j] = true;
+        }
+        else if (dir == 4)
+        {
+            m_maze_table[i - 1, j] = true;
+        }
+    }
+
+    private void DeleteWalls()
+    {
+        for(int i = 0;i < m_array_size; i++)
+        {
+            for(int j = 0;j < m_array_size ; j++)
+            {
+                Destroy(m_wall_objects[i,j]);
+            }
+        }
+    }
+}
---
+++ Assets/Sample/Player/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private float m_MoveSpeed;
+    private Vector3 m_Startpos;
+    private Rigidbody m_RigidBody;
+    private Transform m_Transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Startpos = transform.position;
+        m_Transform = this.transform;
+        m_RigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //プレイヤーの移動
+        TryRun();
+        //リスタート
+        TryRestart();
+    }
+
+    private void TryRun()
+    {
+        Vector3 moveDirection;
+
+        //移動方向とアニメーションの決定
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveDirection = new Vector3(-m_MoveSpeed * Time.deltaTime, 0, 0);
+            m_Animator.SetBool("Run", true);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDirection = new Vector3(m_MoveSpeed * Time.deltaTime, 0, 0);
+            m_Animator.SetBool("Run", true);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveDirection = new Vector3(0, 0, m_MoveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveDirection = new Vector3(0, 0, -m_MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            moveDirection = new Vector3(0, 0, 0);
+            m_Animator.SetBool("Run", false);
+        }
+        //見る方向を変える
+        m_Transform.LookAt(m_Transform.position + moveDirection);
+
+        //前後には動かないように
+        moveDirection.z = 0;
+
+        //移動
+        m_RigidBody.MovePosition(m_Transform.position + moveDirection);
+        
+
+    }
+
+    private void TryRestart()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = m_Startpos;
+            GetComponent<Rigidbody>().velocity *= 0;
+        }
+    }
+    
+}
