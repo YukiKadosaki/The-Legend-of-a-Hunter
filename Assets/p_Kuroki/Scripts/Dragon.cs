06453b864f1abb327a2ffc96a430c65da983152b
Kuroki <61261983+Kuroki-Togo@users.noreply.github.com>
2020-10-29 20:47:03 +0900

    変更

---
+++ Assets/p_Kuroki/Scripts/Dragon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : Boss
+{
+    [SerializeField] private Vector3 dest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        StartCoroutine(MoveLiner(dest));
+        Debug.Log("End");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override IEnumerator MoveToDestination (Vector3 destination)
+    {
+        yield return null;
+    }
+}
