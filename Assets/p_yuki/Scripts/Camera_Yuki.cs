686f2b6f13317bc99e2c9d5c9782b27ceb9ec465
YukiKadosaki <61261945+YukiKadosaki@users.noreply.github.com>
2020-11-19 04:56:16 +0900

    Player Model

---
+++ Assets/p_yuki/Scripts/Camera_Yuki.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Yuki : MonoBehaviour
+{
+    [SerializeField] private Transform m_player;
+    [SerializeField] private Transform m_camera;
+    private Vector3 m_CameraOffset;
+    private Vector3 m_CameraToPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CameraOffset = m_camera.localPosition - m_player.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_CameraToPlayer = (m_player.localPosition - this.transform.localPosition);
+        this.transform.position = m_player.localPosition + m_CameraOffset;
+        this.transform.LookAt(m_player);
+        Debug.Log("this.transform.position:" + this.transform.localPosition);
+    }
+}
--- Assets/p_yuki/Scripts/Player_Yuki.cs
+++ Assets/p_yuki/Scripts/Player_Yuki.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player_Yuki : MonoBehaviour
 {
-    [SerializeField] Camera m_camera;//Cinemachineのカメラ
+    [SerializeField] Transform m_camera;//Cinemachineのカメラ
     [SerializeField] float m_walkSpeed;//歩く速度
     [SerializeField] float m_runSpeed;//走る速度
+    [SerializeField] float m_rotSpeed;//回転速度
     Rigidbody m_rigidBody;
     float m_MoveFront;//GetAxisで手に入れる前へ進む速度の割合(0~1）
     float m_MoveSide;//GetAxisで手に入れる横へ進む速度の割合(0~1)
-    float theta;//CmaeraToPlayerとMoveDirectionのなす角
+    float m_rotRadius;//回転半径
+    Vector3 m_PlayerPosition;//極座標でのプレイヤーの座標
+    Vector3 m_rotCenter;//回転中心
     Vector3 m_MoveDirection;//相対的な（カメラからプレイヤーへのベクトルを基準とした)移動方向
     Vector3 m_MoveDirectionAbs;//絶対座標での移動方向
     Vector3 m_cameraToPlayer;//カメラからプレイヤーへの方向を表すベクトルの単位ベクトル
@@ -30,29 +33,37 @@ public class Player_Yuki : MonoBehaviour
         m_MoveDirection = new Vector3(m_MoveSide, 0, m_MoveFront).normalized;
 
         m_MoveDirectionAbs = Vector3.zero;
+
         //ボタンを押していない場合はスキップ
         if (m_MoveDirection.sqrMagnitude != 0)
         {
             //カメラからプレイヤーまでのベクトルを計算
-            m_cameraToPlayer = this.transform.localPosition - m_camera.transform.localPosition;
+            m_cameraToPlayer = this.transform.localPosition - m_camera.localPosition;
             m_cameraToPlayer.y = 0;
-            m_cameraToPlayer = m_cameraToPlayer.normalized;
 
             //CmaeraToPlayerとMoveDirectionの内積からなす角を求める
-            theta = Vector3.Angle(m_cameraToPlayer, m_MoveDirection);
+            float theta = Vector3.Angle(m_cameraToPlayer.normalized, m_MoveDirection);//CmaeraToPlayerとMoveDirectionのなす角
 
-            m_MoveDirectionAbs = Quaternion.Euler(0, theta, 0) * m_cameraToPlayer;
+            //回転半径、原点を求める
+            m_rotRadius = m_cameraToPlayer.magnitude / Mathf.Sin(theta);
+            m_rotCenter = Quaternion.Euler(0, 90, 0) * m_MoveDirection *  m_rotRadius;
 
-            Debug.Log("m_MoveDirection:" + m_MoveDirection);
-            Debug.Log("m_cameraToPlayer:" + m_cameraToPlayer);
-            Debug.Log("m_MoveDirectionAbs:" + m_MoveDirectionAbs);
-            Debug.Log("theta:" + theta);
+            m_PlayerPosition = m_rotCenter + new Vector3(m_rotRadius * Mathf.Cos(Mathf.Deg2Rad * theta), 0, m_rotRadius * Mathf.Sin(Mathf.Deg2Rad * theta));
+            Vector3 nextpos = m_rotCenter + new Vector3(m_rotRadius * Mathf.Cos(Mathf.Deg2Rad *(theta + m_rotSpeed)), 0, m_rotRadius * Mathf.Sin(Mathf.Deg2Rad * (theta + m_rotSpeed)));
+            m_MoveDirectionAbs = nextpos - m_PlayerPosition;
+
+            this.transform.localPosition = m_PlayerPosition + m_MoveDirectionAbs;
+
+            //Debug.Log("m_PlayerPosition:" + m_PlayerPosition);
+            Debug.Log("nextpos:" + nextpos);
+            Debug.Log("m_rotCenter:" + m_rotCenter);
+
         }
     }
 
     private void FixedUpdate()
     {
-        m_rigidBody.AddForce(m_MoveDirectionAbs * m_walkSpeed);
+        //m_rigidBody.AddForce(m_MoveDirectionAbs * m_walkSpeed);
     }
 
     
