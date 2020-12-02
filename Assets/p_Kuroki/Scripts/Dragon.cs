0f07434795f7eb92d03d2723b64399b5580627fd
Kuroki <61261983+Kuroki-Togo@users.noreply.github.com>
2020-12-02 20:22:30 +0900

    変更

--- Assets/p_Kuroki/Scripts/Dragon.cs
+++ Assets/p_Kuroki/Scripts/Dragon.cs
@@ -19,23 +19,53 @@ public class Dragon : Boss
         //Debug.Log("End");
         
         //自機のオブジェクトを見つける
-        player = GameObject.Find("Player");
+        player = GameObject.Find("PlayerSample");
+        Debug.Log("target = " + player.name);
 
         PlayerPosition = player.transform.position;
-        EnemyPosition = transform.position;
+        EnemyPosition = this.transform.position;       
     }
 
     void Update()
     {
-        while(Hp > 0)
-        {
-            currentTime += Time.deltaTime;
-            if (targetTime < currentTime)
-            {
-                currentTime = 0;
-            }
-        }
+        //while(Hp > 0)
+        //{
+
+        //InvokeRepeating("Move", 2f, 10f);   //2秒後に関数Moveを実行し、3秒間隔で続ける
+        Invoke("Move", 2f);
+        //}
         //yield break;  //コルーチン終了
+    }
+
+    void Move()
+    {
+        this.transform.LookAt(player.transform);  //オブジェクトをプレイヤーの位置に向かせる
+        StartCoroutine("Stop");
+        
+        float step = MoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, PlayerPosition, step);
+
+        Invoke("Turn", 2f);
+        //自機のオブジェクトを見つける
+        //player = GameObject.Find("PlayerSample");
+        //Debug.Log("target = " + player.name);
+        //PlayerPosition = player.transform.position;
+        //EnemyPosition = this.transform.position;
+    }
+
+    void Turn()
+    {
+        //自機のオブジェクトを見つける
+        //player = GameObject.Find("PlayerSample");
+        //Debug.Log("target = " + player.name);
+        PlayerPosition = player.transform.position;
+        EnemyPosition = this.transform.position;
+        Invoke("Turn", 2f);
+    }
+
+    IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(2);  //2秒間処理を止める
     }
 
     //目的地(destination）に直線移動する
