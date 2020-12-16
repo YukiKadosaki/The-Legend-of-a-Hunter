be79f5bb25aefa48782f5b00166de611db0ab897
Kuroki <61261983+Kuroki-Togo@users.noreply.github.com>
2020-12-16 21:11:57 +0900

    変更

--- Assets/p_Kuroki/Scripts/Dragon.cs
+++ Assets/p_Kuroki/Scripts/Dragon.cs
@@ -10,15 +10,14 @@ public class Dragon : Boss
     private Vector3 PlayerPosition; //プレイヤーの位置情報
     private Vector3 EnemyPosition; //敵の位置情報
     private float distance;  //プレイヤーと敵の距離
-    private float targetTime = 1.0f;
-    private float currentTime = 0;
+    //private float targetTime = 1.0f;
+    //private float currentTime = 0;
 
     void Start()
     {
-        //Debug.Log("Start");   
+         
         //StartCoroutine(MoveLiner(dest));  //コルーチン(MoveLiner)を実行
-        //Debug.Log("End");
-        
+               
         //自機のオブジェクトを見つける
         player = GameObject.Find("PlayerSample");
         Debug.Log("target = " + player.name);
@@ -31,43 +30,62 @@ public class Dragon : Boss
     {
         //while(Hp > 0)
         //{
-
         //InvokeRepeating("Move", 2f, 10f);   //2秒後に関数Moveを実行し、3秒間隔で続ける
-        distance = Vector3.Distance(PlayerPosition, transform.position);
+        //distance = Vector3.Distance(PlayerPosition, transform.position);
         Invoke("Move", 2f);
+        //StartCoroutine("Move");
         //}
         //yield break;  //コルーチン終了
     }
 
+    //IEnumerator Move()
     void Move()
     {
+        PlayerPosition = player.transform.position;
+        EnemyPosition = this.transform.position;
+        distance = Vector3.Distance(PlayerPosition, transform.position);
+        //Debug.Log("distance = " + distance);
         this.transform.LookAt(player.transform);  //オブジェクトをプレイヤーの位置に向かせる
         StartCoroutine("Stop");  //コルーチン"Stop"を起動
-
+        
         if (distance > 2.0) 
         {
             float step = MoveSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, PlayerPosition, step);
+            if(distance <= 2.0)
+            {
+                CancelInvoke(); //実行しているInvoke(ここでは、Move)を停止              
+                Invoke("Turn", 1f);
+                //StartCoroutine("Turn");
+                //yield break;
+            }
         }      
         else{
+            CancelInvoke(); //実行しているInvoke(ここでは、Move)を停止        
             Invoke("Turn", 2f);
+            //StartCoroutine("Turn");
+            //yield break;
         }
-        
+
     }
 
+
+    //IEnumerator Turn()
     void Turn()
     {
-        //自機のオブジェクトを見つける
-        
-        ///PlayerPosition = player.transform.position;
-        //EnemyPosition = this.transform.position;
-       
+        Debug.Log("Turn Mode");
+        distance = 0;
+        StartCoroutine("Stop");  //コルーチン"Stop"を起動
+        CancelInvoke();
         Invoke("Move", 10f);
+        //StartCoroutine("Move");
+        //yield break;
     }
 
     IEnumerator Stop()
     {
         yield return new WaitForSeconds(2);  //2秒間処理を止める
+        yield break; 
     }
 
     //目的地(destination）に直線移動する
