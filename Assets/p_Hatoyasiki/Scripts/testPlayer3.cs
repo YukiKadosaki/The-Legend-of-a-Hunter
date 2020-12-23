e9e9b6dd9fb754dfce3af0871b04425f672fd43b
PigeonsHouse <bullbull1230@gmail.com>
2020-12-23 22:18:54 +0900

    [UPDATE] Kボタンでカメラが背後に回る機能を追加

--- Assets/p_Hatoyasiki/Scripts/testPlayer3.cs
+++ Assets/p_Hatoyasiki/Scripts/testPlayer3.cs
@@ -20,6 +20,8 @@ public class testPlayer3 : MonoBehaviour
     private Vector2 playerVec2;
     private Vector2 cameraVec2;
     private Vector2 criteriaVec2;
+    private Vector2 moveVec2;
+    private Vector3 moveVec3;
     private float const_distance;
     private bool m_OnLand
     {
@@ -42,71 +44,80 @@ public class testPlayer3 : MonoBehaviour
         cameraVec2 = new Vector2(camera.transform.position.x, camera.transform.position.z);
         criteriaVec2 = playerVec2 - cameraVec2;
         const_distance = criteriaVec2.magnitude;
+        moveVec2 = Vector2.zero;
     }
 
     // Update is called once per frame
+
+    // time.deltatimeを追加することを考える．
     void Update(){
         camera.transform.LookAt(this.transform);
 
         // 無効入力をスルー
-        if (!((Input.GetKey(KeyCode.W) ^ Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D))))
-            return;
+        if ((Input.GetKey(KeyCode.W) ^ Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D))){
+            // 基準ベクトルの取得
+            playerVec2 = new Vector2(this.transform.position.x, this.transform.position.z);
+            cameraVec2 = new Vector2(camera.transform.position.x, camera.transform.position.z);
+            criteriaVec2 = playerVec2 - cameraVec2;
 
-        // 基準ベクトルの取得
-        playerVec2 = new Vector2(this.transform.position.x, this.transform.position.z);
-        cameraVec2 = new Vector2(camera.transform.position.x, camera.transform.position.z);
-        criteriaVec2 = playerVec2 - cameraVec2;
+            // ここ見直す
+            Vector2 velocityVec2 = m_MoveSpeed * criteriaVec2.normalized;
+            var vec = velocityVec2;
+            Vector2 lotateVec2 = Quaternion.Euler( 0, 0, 90 ) * vec;
 
-        Vector2 velocityVec2 = m_MoveSpeed * criteriaVec2.normalized;
-        var vec = velocityVec2;
-        Vector2 lotateVec2 = Quaternion.Euler( 0, 0, 90 ) * vec;
+            List<Vector2> vec2list = new List<Vector2>();
 
-        List<Vector2> vec2list = new List<Vector2>();
+            bool go_forward     = Input.GetKey(KeyCode.W);
+            bool go_backward    = Input.GetKey(KeyCode.S);
+            bool go_left        = Input.GetKey(KeyCode.A);
+            bool go_right       = Input.GetKey(KeyCode.D);
 
-        bool go_forward     = Input.GetKey(KeyCode.W);
-        bool go_backward    = Input.GetKey(KeyCode.S);
-        bool go_left        = Input.GetKey(KeyCode.A);
-        bool go_right       = Input.GetKey(KeyCode.D);
+            if(go_forward){
+                playerVec2 += velocityVec2;
+                cameraVec2 += velocityVec2;
+                vec2list.Add(velocityVec2);
+            }
+            if(go_backward){
+                playerVec2 -= velocityVec2;
+                cameraVec2 -= velocityVec2;
+                vec2list.Add(-velocityVec2);
+            }
+            if(go_left){
+                playerVec2 += lotateVec2;
+                vec2list.Add(lotateVec2);
+            }
+            if(go_right){
+                playerVec2 -= lotateVec2;
+                vec2list.Add(-lotateVec2);
+            }
 
-        if(go_forward){
-            playerVec2 += velocityVec2;
-            cameraVec2 += velocityVec2;
-            vec2list.Add(velocityVec2);
+
+            moveVec2 = Vector2.zero;
+            foreach (Vector2 v in vec2list){
+                moveVec2 += v;
+            }
+            moveVec3 = new Vector3(moveVec2.x, 0, moveVec2.y);
+
+            Vector2 new_criteriaVec2 = playerVec2 - cameraVec2;
+            float difference = new_criteriaVec2.magnitude - criteriaVec2.magnitude;
+            Vector2 diffeVec2 = difference * new_criteriaVec2.normalized;
+            playerVec2 -= diffeVec2;
+
+            Vector2 neo_criteriaVec2 = playerVec2 - cameraVec2;
+            float distance_difference = neo_criteriaVec2.magnitude - const_distance;
+            cameraVec2 += distance_difference * neo_criteriaVec2.normalized;
+
+            this.transform.position = new Vector3(playerVec2.x, this.transform.position.y, playerVec2.y);
+            camera.transform.position = new Vector3(cameraVec2.x, camera.transform.position.y, cameraVec2.y);
+
+            camera.transform.LookAt(this.transform);
+            this.transform.LookAt(this.transform.position + moveVec3);
         }
-        if(go_backward){
-            playerVec2 -= velocityVec2;
-            cameraVec2 -= velocityVec2;
-            vec2list.Add(-velocityVec2);
+
+        if(Input.GetKeyDown(KeyCode.K)){
+            Vector3 newCameraVector3 = this.transform.position - moveVec3.normalized * const_distance;
+            camera.transform.position = new Vector3(newCameraVector3.x, camera.transform.position.y, newCameraVector3.z);
+            camera.transform.LookAt(this.transform);
         }
-        if(go_left){
-            playerVec2 += lotateVec2;
-            vec2list.Add(lotateVec2);
-        }
-        if(go_right){
-            playerVec2 -= lotateVec2;
-            vec2list.Add(-lotateVec2);
-        }
-
-
-        Vector2 moveVec2 = Vector2.zero;
-        foreach (Vector2 v in vec2list){
-            moveVec2 += v;
-        }
-        Vector3 moveVec3 = new Vector3(moveVec2.x, 0, moveVec2.y);
-
-        Vector2 new_criteriaVec2 = playerVec2 - cameraVec2;
-        float difference = new_criteriaVec2.magnitude - criteriaVec2.magnitude;
-        Vector2 diffeVec2 = difference * new_criteriaVec2.normalized;
-        playerVec2 -= diffeVec2;
-
-        Vector2 neo_criteriaVec2 = playerVec2 - cameraVec2;
-        float distance_difference = neo_criteriaVec2.magnitude - const_distance;
-        cameraVec2 += distance_difference * neo_criteriaVec2.normalized;
-
-        this.transform.position = new Vector3(playerVec2.x, this.transform.position.y, playerVec2.y);
-        camera.transform.position = new Vector3(cameraVec2.x, camera.transform.position.y, cameraVec2.y);
-
-        camera.transform.LookAt(this.transform);
-        this.transform.LookAt(this.transform.position + moveVec3);
     }
 }
