c4621b2d7085d352258124e0f65fbb05da9860bd
YukiKadosaki <61261945+YukiKadosaki@users.noreply.github.com>
2021-01-20 21:45:41 +0900

    [Add]触れると音を発するオブジェクト

---
+++ Assets/p_yuki/Scripts/Speaker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speaker : MonoBehaviour
+{
+    AudioSource m_Audio;
+
+    private void Start()
+    {
+        m_Audio = this.GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Touch");
+        if (other.CompareTag("Player"))
+        {
+            m_Audio.Play();
+            Debug.Log("Play!");
+        }
+    }
+}
