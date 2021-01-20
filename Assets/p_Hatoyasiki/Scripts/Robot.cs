669ba7d379a314229ba87091fc19db546e7e584f
PigeonsHouse <bullbull1230@gmail.com>
2021-01-20 11:51:32 +0900

    [ADD] ロボットの経路探索用ウェイポイントの追加

--- Assets/p_Hatoyasiki/Scripts/Robot.cs
+++ Assets/p_Hatoyasiki/Scripts/Robot.cs
@@ -8,22 +8,33 @@ public class Robot : Boss
     public static int ROW = 13;
     public GameObject player;
     public Vector3 pos;
+    public Material BossColor;
+    public Material WPColor;
     private int[,] terrainInfo = new int[COLUMN, ROW];
     private bool isGrasp = false;
     private bool isRunning = false;
+    private GameObject PlayerWP;
+    private float WPReloadTime = 0;
 
-    void Start()
-    {
+    void Start() {
+        PlayerWP = serchTag(gameObject, "WP");
+        PlayerWP.GetComponent<Renderer>().material.color = BossColor.color;
     }
 
-    void Update()
-    {
+    void Update() {
+        WPReloadTime += Time.deltaTime;
+        if(WPReloadTime >= 0.5f){
+            PlayerWP.GetComponent<Renderer>().material.color = WPColor.color;
+            PlayerWP = serchTag(gameObject, "WP");
+            PlayerWP.GetComponent<Renderer>().material.color = BossColor.color;
+            WPReloadTime = 0f;
+        }
         StartCoroutine(MoveToDestination(player.transform.position));
-        // if(!isGrasp){
-        //     Terrain_grasp();
-        // }else{
-
-        // }
+        if(!isGrasp){
+            Terrain_grasp();
+        }else{
+            ;
+        }
     }
     //目的地（destination）に障害物などを避けながら移動する 
     public override IEnumerator MoveToDestination(Vector3 destination)
@@ -75,5 +86,29 @@ public class Robot : Boss
             text += "\n";
         }
         Debug.Log(text);
+    }
+    GameObject serchTag(GameObject nowObj,string tagName){
+        float tmpDis = 0;           //距離用一時変数
+        float nearDis = 0;          //最も近いオブジェクトの距離
+        //string nearObjName = "";    //オブジェクト名称
+        GameObject targetObj = null; //オブジェクト
+
+        //タグ指定されたオブジェクトを配列で取得する
+        foreach (GameObject obs in  GameObject.FindGameObjectsWithTag(tagName)){
+            //自身と取得したオブジェクトの距離を取得
+            tmpDis = Vector3.Distance(obs.transform.position, nowObj.transform.position);
+
+            //オブジェクトの距離が近いか、距離0であればオブジェクト名を取得
+            //一時変数に距離を格納
+            if (nearDis == 0 || nearDis > tmpDis){
+                nearDis = tmpDis;
+                //nearObjName = obs.name;
+                targetObj = obs;
+            }
+
+        }
+        //最も近かったオブジェクトを返す
+        //return GameObject.Find(nearObjName);
+        return targetObj;
     }
 }
