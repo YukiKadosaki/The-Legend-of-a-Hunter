268723989bbbfb7f43f5b969c976a90c89454ec8
YukiKadosaki <61261945+YukiKadosaki@users.noreply.github.com>
2021-02-17 20:15:50 +0900

    [Test]蜂が一定間隔で攻撃の動きをする

---
+++ Assets/p_yuki/Scripts/TestScript/Hati_Yuki.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hati_Yuki : MobStatus
+{
+    private Animator _anim;
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        _anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(2 <= time && time <= 4)
+        {
+            _anim.SetBool("Attack", true);
+        }
+        else if(4 <= time)
+        {
+            time = 0;
+        }
+        else
+        {
+            _anim.SetBool("Attack", false);
+        }
+
+        time += Time.deltaTime;
+    }
+}
