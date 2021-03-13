4d0e60ab5c2bf9fb573aa3289ffc02cd7611cb70
Dan <65810344+Daaaaaaan3229@users.noreply.github.com>
2021-03-13 10:55:54 +0900

    蜂、狩人を追いかける

---
+++ Assets/p_Dan_/Scripts/MushiMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MushiMove : MonoBehaviour
+{
+
+    private NavMeshAgent _agent;
+    private RaycastHit[] _raycastHits = new RaycastHit[10];
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    //CollisionDetectorのonTriggersStayにセットし、衝突判定を受け取るメソッド
+    public void OnDetectObject(Collider collider)
+    {
+        //検知したオブジェクトに「player」のタグがついていれば、そのオブジェクトを追いかける
+        if (collider.CompareTag("Player"))
+        {
+            //自身とプレイヤーの座標差分を計算
+            var positionDiff = collider.transform.position - transform.position;
+            //プレイヤーとの距離を計算
+            var distance = positionDiff.magnitude;
+            //プレイヤーへの方向
+            var direction = positionDiff.normalized;
+            //_rayvastHitsに、ヒットしたcolliderや座標情報などが格納される
+            var hitCount = Physics.RaycastNonAlloc(transform.position, direction, _raycastHits, distance);
+            Debug.Log("hitCount:" + hitCount);
+            if (hitCount != 0)
+            {
+                _agent.isStopped = false;
+                _agent.destination = collider.transform.position;
+            }
+            else
+            {
+                _agent.isStopped = true;
+            }
+
+            
+        }
+    }
+}
