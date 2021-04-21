88cd0913cb0971e5e87ae1f15e3d1dd0b3effe34
Dan <65810344+Daaaaaaan3229@users.noreply.github.com>
2021-04-21 20:56:26 +0900

    mushiugoitayon

---
+++ Assets/p_Dan_/Scripts/BeeMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class BeeMove : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>(); // NavMeshAgentを保持しておく
+    }
+
+    // CollisionDetector.csのonTriggerStayにセットし、衝突中に実行される。
+    public void OnDetectObject(Collider collider)
+    {
+        // 検知したオブジェクトに「Player」のタグがついていれば、そのオブジェクトを追いかける
+        if (collider.CompareTag("Player"))
+        {
+            navMeshAgent.destination = collider.transform.position;
+        }
+    }
+}
