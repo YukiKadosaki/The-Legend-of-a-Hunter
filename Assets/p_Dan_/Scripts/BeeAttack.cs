ff6fdfb43ce95e0150744fef10c629a1f01a1680
Dan <65810344+Daaaaaaan3229@users.noreply.github.com>
2021-04-28 20:22:11 +0900

    クララが立った

---
+++ Assets/p_Dan_/Scripts/BeeAttack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//攻撃制御クラス
+[RequireComponent(typeof(MobStatus))]
+public class BeeAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown = 0.5f;//攻撃後のクールダウン(秒)
+    [SerializeField] private Collider attackCollider;
+
+    private MobStatus _status;
+
+    private void Start()
+    {
+        _status = GetComponent<MobStatus>();
+    }
+
+    //攻撃可能な状態であれば攻撃を行います
+    public void AttackIfPossible()
+    {
+        if (!_status.IsAttackable) return;
+        //ステータスと衝突したオブジェクトで攻撃可否を判断
+
+        _status.GoToAttackStateIfPossible();
+    }
+
+    //攻撃対象が攻撃範囲に入った時に呼ばれます
+    ///<param name="collider"></param>
+    public void OnAttackRangeEnter(Collider collider)
+    {
+        AttackIfPossible();
+    }
+
+    //攻撃の開始時に呼ばれます
+    public void OnAttackStart()
+    {
+        attackCollider.enabled = true;
+    }
+
+    //attackColliderが攻撃対象にHitした時に呼ばれます
+    ///<param name="collider"></param>
+    public void OnHitAttack(Collider collider)
+    {
+        var targetMob = collider.GetComponent<MobStatus>();
+        if (null == targetMob) return;
+
+        //プレイヤーにダメージを与える
+        targetMob.Damage(1);
+    }
+
+    //攻撃の終了時に呼ばれます
+    public void OnAttackFinished()
+    {
+        attackCollider.enabled = false;
+        StartCoroutine(CooldownCoroutine());
+    }
+
+    private IEnumerator CooldownCoroutine()
+    {
+        yield return new WaitForSeconds(attackCooldown);
+        _status.GoToNormalStateIfPossible();
+    }
+
+}
---
+++ Assets/p_Dan_/Scripts/BeeStatus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+//蜂の状態管理スクリプト
+[RequireComponent(typeof(NavMeshAgent))]
+public class BeeStatus : MobStatus
+{
+    private NavMeshAgent _agent;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //NavMeshAgentのvelocityで移動速度のベクトルが取得できる
+        _animator.SetFloat("MoveSpeed", _agent.velocity.magnitude);
+    }
+
+    protected override void OnDie()
+    {
+        base.OnDie();
+        StartCoroutine(DestroyCoroutine());
+    }
+
+    //倒された時の消滅コルーチンです
+    /// <returns></returns>
+    private IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
