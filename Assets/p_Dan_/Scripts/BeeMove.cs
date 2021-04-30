a22046eee8c8e6fa8df2fa1ecf76961655565e43
Dan <65810344+Daaaaaaan3229@users.noreply.github.com>
2021-04-30 19:26:05 +0900

    子蜂、プレイヤー

--- Assets/p_Dan_/Scripts/BeeMove.cs
+++ Assets/p_Dan_/Scripts/BeeMove.cs
@@ -2,18 +2,27 @@
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(BeeStatus))]
 public class BeeMove : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
 
+    private BeeStatus _status;
+
     private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>(); // NavMeshAgentを保持しておく
+        _status = GetComponent<BeeStatus>();
     }
 
     // CollisionDetector.csのonTriggerStayにセットし、衝突中に実行される。
     public void OnDetectObject(Collider collider)
     {
+        if(!_status.IsMovable)
+        {
+            navMeshAgent.isStopped = true;
+            return;
+        }
         // 検知したオブジェクトに「Player」のタグがついていれば、そのオブジェクトを追いかける
         if (collider.CompareTag("Player"))
         {
